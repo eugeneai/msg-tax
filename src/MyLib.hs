3a021229ad096b4653a3a97116ee932df0a463ff
Evgeny Cherkashin <eugeneai@irnok.net>
2023-04-18 21:22:33 +0800

    Adjusting engine

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -136,7 +136,10 @@ instance ToJSON Morph
 
 instance ToJSON TagSet where
 
-  toJSON (TagSet s) = Array $ V.empty
+  toJSON :: TagSet -> Value
+  toJSON (TagSet s) = Array . V.fromList . map f . Set.toList $ s
+    where
+      f gram = String . T.pack . show $ gram
 
 instance ToJSON MorphTag
 
@@ -179,27 +182,20 @@ version = "0.0.1"
 data Connection = AdjNoun | SubjVerb | NounNounGent
   deriving Show
 
-data Join = R [Join] | J Connection (Morph, Morph) (Join, Join) | L T.Text [Morph]
+data Join = R [Join] | J Connection Join Join | M [Morph]
 
 instance Show Join where
-  show (J conn m (a, b)) = "(J " ++ show conn ++ " " ++ tail ++ " / "++ show m ++ ")\n"
+  show (J c a b) = "(J " ++ show c ++ " " ++ tails ++ ")\n"
     where
-      tail = show a ++ " " ++ show b
+      tails = show a ++ " " ++ show b
   show (R ts) = "(R " ++ (concatMap show $ ts) ++ ")"
-  show (L u ts) = "|" ++ (T.unpack u) ++
-    "\n"
---    " " ++ show ts ++ "\n"
+  show (M ts) = "|" ++ show ts ++ "\n"
 
 toJoin :: Maybe Message -> Maybe Join
 toJoin Nothing = Nothing
-toJoin (Just msg) = Just . R . map lex . text $ msg
+toJoin (Just msg) = Just . R . map lext . text $ msg
   where
-    lex l = L (ucto l) (maybe [] id $ morph l)
-    -- mynorm ::Lex -> T.Text
-    -- mynorm l = maybe (w l) norm (morph l)
-    -- mytagset :: Lex -> TagSet
-    -- mytagset l = maybe (TagSet Set.empty) tag (morph l)
-
+    lext l = M (maybe [] id $ morph l)
 
 class Rule r where
   join :: r -> (Morph->Morph->Bool, Morph->Bool, Morph->Bool)
@@ -215,9 +211,15 @@ applyRule r (a:b:l) = rc
   where
     (mjoin, left, right) = join r
     lm = [ (ma, mb) | (ma, mb) <- compPairs (left, right) a b, mjoin ma mb ]
-    nj = J r (head lm) (a, b)
+    morphs = head lm
+    nj = red (a, b) morphs
     rc = if null lm then a:(applyRule r (b:l))
          else applyRule r (nj:l)
+    red :: (Join, Join) -> (Morph, Morph) -> Join
+    red (M _,     M _)     (ma, mb) = J r (M [ma]) (M [mb])
+    red (M _,     J c a b) (ma, _)  = J r (M [ma]) (J c a b)
+    red (J c a b, M _)     (_,  mb) = J r (J c a b) (M [mb])
+    red (a,       b)       (_,  _)  = J r a b
 
 data GRAM = BAD
   | POST  | NOUN  | ADJF  | ADJS  | COMP  | VERB  | INFN  | PRTF  | PRTS
@@ -232,6 +234,7 @@ data GRAM = BAD
   | SLNG  | ARCH  | LITR  | ERRO  | DIST  | QUES  | DMNS  | PRNT  | V_BE  | V_EN
   | V_IE  | V_BI  | FIMP  | PRDX  | COUN  | COLL  | V_SH  | AF_P  | INMX  | VPRE
   | ANPH  | INIT  | ADJX  | HYPO  | LATN  | UNKN
+  | PRONOUN
   | Unrec T.Text -- For all unknown
   deriving (Show, Read, Ord, Eq)
 
@@ -261,10 +264,11 @@ compPairs (left, right) a b = rc
     f :: (Float, Morph, Morph) -> (Float, Morph, Morph) -> Ordering
     f (a', _, _) (b', _, _) = compare a' b'
     getParJ (R l) = getParL l
-    getParJ (J _ _ (a, _)) = getParJ a
-    getParJ (L _ morphs) = morphs
+    getParJ (J _ aj bj) = getParJ aj ++ getParJ bj
+    getParJ (M morphs) = morphs
     getParL [] = []
     getParL (p:_) = getParJ p
+
     calcS :: Morph -> Morph -> Float
     calcS a1 b1 = (score a1) * (score b1)
 
@@ -314,14 +318,12 @@ nounCases = Set.fromList
 mult :: Set.Set GRAM
 mult = Set.fromList [SING, PLUR]
 
-data POSAttr = PMULT | PCASE | PPRON
-
-getProp :: [POSAttr] -> Morph -> Set.Set GRAM
-getProp [PCASE] m = Set.filter f $ unTS . tag $ m
+getProp :: [GRAM] -> Morph -> Set.Set GRAM
+getProp [CASE] m = Set.filter f $ unTS . tag $ m
   where f e = Set.member e nounCases
-getProp [PMULT] m = Set.filter f $ unTS . tag $ m
+getProp [MULT] m = Set.filter f $ unTS . tag $ m
   where f e = Set.member e mult
-getProp [PPRON] m = Set.filter f $ unTS . tag $ m
+getProp [PRONOUN] m = Set.filter f $ unTS . tag $ m
   where f e = Set.member e pronouns
 getProp [] _ = Set.empty:: Set.Set GRAM
 getProp (a:t) ts = getProp [a] ts `Set.union` getProp t ts
@@ -332,7 +334,7 @@ unTS (TagSet t) = t
 sameDeclination :: Morph -> Morph -> Bool
 sameDeclination a b = sa =*= sb
   where
-    as = [PCASE, PMULT]
+    as = [CASE, MULT]
     sa = getProp as a
     sb = getProp as b
 
@@ -350,10 +352,10 @@ isPronoun p = any f $ Set.toList pronouns
 subjVerbConsist :: Morph -> Morph -> Bool
 subjVerbConsist subj verb = pronVerb || nounVerb
   where
-    pra = [PPRON, PMULT]
-    va1 = [PPRON, PMULT]
-    va2 = [PMULT]
-    na = [PMULT]
+    pra = [PRONOUN, MULT]
+    va1 = [PRONOUN, MULT]
+    va2 = [MULT]
+    na = [MULT]
     ppr = getProp pra subj
     vpr1 = getProp va1 verb
     vpr2 = getProp va2 verb
